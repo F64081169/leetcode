69d11ac5d5c90cecc37345a11bbff3655cfff57b
Cat Burglar <62001405+F64081169@users.noreply.github.com>
2024-03-27 11:03:35 +0800

    Update 409.cpp

--- easy/409.cpp
+++ easy/409.cpp
@@ -14,3 +14,20 @@ public:
         return (!m.empty())? result+1:result;
     }
 };
+/*
+// faster sol
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        // greedy count the frequency of each alphabet
+        vector<int> freq(128);
+        int result = 0,odd = 0;
+        for(char c:s) freq[c]++;
+        for(int i=0;i<128;i++){
+            result += freq[i]&(~1);
+            if(freq[i]&1)odd=1;
+        } 
+        return result+odd;
+    }
+};
+*/
