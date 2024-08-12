348183220d785eddd27fd1dc75a7c59cd6af2e86
Cat Burglar <62001405+F64081169@users.noreply.github.com>
2024-08-12 11:01:53 +0800

    Create 56.cpp

---
+++ medium/56.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[0] < b[0];
+        });
+        vector<vector<int>> res;
+        for (int i = 0; i < intervals.size(); i++) {
+            if (res.empty() || res.back()[1] < intervals[i][0]) {
+                res.push_back(intervals[i]);
+            } else {
+                res.back()[1] = max(res.back()[1], intervals[i][1]);
+            }
+        }
+        return res;
+    }
+};
